7097bc3fa2548bdf380423889938060aa602ebdd
andhip <andhi.puspianto10@gmail.com>
2021-02-28 01:32:42 +0700

    update 2 files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("Author - Andy Nhov");
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
